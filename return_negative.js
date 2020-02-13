c4af40ede53391e967503886da50c19ea62d2723
Crew Spence <csspence123@gmail.com>
2020-02-13 06:48:52 -0600

    Wrote naive solution for return_negative.

--- return_negative.js
+++ return_negative.js
@@ -13,6 +13,14 @@ The number can be negative already, in which case no change is required.
 Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
 */
 
-function makeNegative(num) {
-  // Code?
+const makeNegative = (num) => {
+  if(num < 0) {
+    return num;
+  }
+  if(num === 0) {
+    return num;
+  }
+  if(num > 0) {
+    return parseInt('-' + num);
+  }
 }
